3bbbe664b9304a947f4f4328fac38452af28433d
Eze323 <103865149+Eze323@users.noreply.github.com>
2022-04-26 23:07:59 -0300

    comitiar

--- funciones.js
+++ funciones.js
@@ -1,3 +1,40 @@
+/*funciones para el login*/
+
+const usuario = document.getElementById("usuario");
+const clave = document.getElementById("clave");
+const boton = document.getElementById("btnIniciarSesion");
+const resultado = document.querySelector(".resultado");
+
+boton.addEventListener("click",(e)=>{
+        e.preventDefault();
+        let error = verificarCampos();
+
+        if(error[0]){
+            resultado.innerHTML = error[1];
+            resultado.classList.add("red");
+        }else{
+            resultado.innerHTML = error[1];
+            resultado.classList.add("green");
+        }
+})
+
+const validarCampos = ()=>{
+    let error=[];
+    if(usuario.value!='Admin' || clave.value!='123456'  ){
+        error[0]=true;
+        error[1]="El Usuario o Clave son incorrectos";
+        
+    }else{
+        error[0]=false;
+        error[1]=""Iniciando sesión""
+    }
+
+    return error;
+
+}
+
+
+
 funcion clearInterval(){
     alert("hola mundo");
 }
