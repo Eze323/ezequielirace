a840a0e2f75ce1593717119f0aa91ef1cc41ff0e
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2022-06-06 22:03:57 -0300

    Add files via upload

--- mapafuncion.js
+++ mapafuncion.js
@@ -1,27 +1,31 @@
-let Equipo ={
-    LOCATION_ID: "1160" , 
-    SCENARIO:"FABRICA", 
-    AREA:"ARMADO", 
-    LOCAL:"A70-FLEXI 1", 
-    LOCAL_DETAIL:"CF1", 
-    NICKNAME:"A70-F1", 
-    IS_MONITORED:"TRUE"
-    };
-
-
-    new Vue({
+ new Vue({
         el:'#app',
         data:{
+           //rutaDeAcceso:'https://eze323.github.io/ezequielirace/Localizacion.json',
+           rutaDeAcceso:'Localizacion.json',
+           equipo:[],
            equipos:[],
         },
         methods:{
            getEquipos: function(){
                self=this;
-               axios.get('https://eze323.github.io/ezequielirace/Output.json').then (response=>{
+               axios.get(this.rutaDeAcceso).then (response=>{
                    self.equipos = response.data;
                    console.log(self.equipos);
                })
-           }
+           },
+           propiedadesItem: function(item) {
+               //this.equipo=this.equipos.filter(item);
+                   
+               //console.log(this.equipo);
+                
+            alert("hiciste click en el equipamento : "+item);
+            /*document.getElementById("escenario").innerHTML= this.equipos[item].SCENARIO;
+            document.getElementById("area").innerHTML= Equipo.AREA;
+            document.getElementById("local").innerHTML= Equipo.LOCAL;
+            document.getElementById("nickname").innerHTML= Equipo.NICKNAME;           */
+           },
+
         },
         mounted:function(){
            this.getEquipos();
@@ -55,12 +59,11 @@ function crearListado(cant){
 }
 */
 
-/*
-const $mapaElementos= document.querySelector("#mapa");
+
+/*const $mapaElementos= document.querySelector("app");
 $mapaElementos.onclick= function(e){
     let item = e.target.id;
     alert("hiciste click en el equipamento : "+item);
-   
 
     document.getElementById("escenario").innerHTML= Equipo.SCENARIO;
     document.getElementById("area").innerHTML= Equipo.AREA;
@@ -70,9 +73,7 @@ $mapaElementos.onclick= function(e){
 
 }
 
-
-crearListado(160);
-
+//crearListado(160);
 */
 
 
